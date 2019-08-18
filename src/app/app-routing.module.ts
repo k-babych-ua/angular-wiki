c9fafef80ea561cdb80cfe634fb1961b6b7660f9
Konstantin Babich <k.babich95@gmail.com>
2019-08-19 02:02:30 +0300

    Routing for new components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { ArticleFullComponent } from './components/article-full/article-full.component';
+import { ArticleEditComponent } from './components/article-edit/article-edit.component';
+import { ArticleNotFoundComponent } from './components/article-not-found/article-not-found.component';
 
 const routes: Routes = [
   {
@@ -9,6 +11,15 @@ const routes: Routes = [
   },
   {
     path: "article/:id", pathMatch: "full", component: ArticleFullComponent
+  },
+  {
+    path: "article/:id/edit", pathMatch: "full", component: ArticleEditComponent
+  },
+  {
+    path: "articles/new", pathMatch: "full", component: ArticleEditComponent
+  },
+  {
+    path: "**", component: ArticleNotFoundComponent
   }
 ];
 
