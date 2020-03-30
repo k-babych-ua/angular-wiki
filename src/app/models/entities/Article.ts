04fc125c7c218ca2e1051db74a0b60790ec218cf
Konstantin Babich <k.babich95@gmail.com>
2020-03-30 21:39:27 +0300

    Class for article object

---
+++ src/app/models/entities/Article.ts
@@ -0,0 +1,26 @@
+import { IArticle } from './IArticle';
+import { ITag } from './ITag';
+
+export class Article implements IArticle {
+    id: number;    
+    title: string;
+    firstParagraph: string;
+    body: string;
+    imageUrl: string;
+    tags: ITag[];
+
+    constructor(article: IArticle) {
+        this.id = article.id;
+        this.title = article.title;
+        this.firstParagraph = article.firstParagraph;
+        this.body = article.body;
+        this.imageUrl = article.imageUrl;
+        this.tags = article.tags;
+    }
+
+    public getTagsString(): string {
+        return this.tags
+            .map(t => t.title)
+            .join(", ");
+    }
+}
